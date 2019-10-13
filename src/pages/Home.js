4fdeb1b7e731302cc7fdbcb73a312e57045f043e
Rohan <rohanpschauhan@gmail.com>
2019-10-13 22:24:46 +0100

    finished home
    completed responsiveness and home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,24 +5,28 @@ import { Link } from 'react-router-dom';
 export default class Home extends Component {
     render() {
         return(
-        <div className="home-page">
-            
-            <div>
-                <h1>Welcome to Rohan's Website!</h1>
-                <br/>
-                <q>"Every great developer you know got there by solving problems they were unqualified to solve until they did it"</q>
-                <br/>
-                <h3> ~ Patrick McKenzie</h3>
-                <br/>                   
-                    
-                <Link to="./work" className="primary-btn">Portfolio</Link>
+
+            <div className="home-page">
+                
+                <div>
+                    <h1>Welcome to Rohan's Website!</h1>
+                    <br/>
+                    <q>"Every great developer you know got there by solving problems they were unqualified to solve until they did it"</q>
+                    <br/>
+                    <br/>
+                    <h3> ~ Patrick McKenzie</h3>
+                    <br/>                   
+                        
+                    <Link to="./work" className="primary-btn">Projects</Link>
+                                    
+                    <Link to="./contact" className="tertiary-btn">Contact</Link>
                                 
-                <Link to="./contact" className="tertiary-btn">Contact</Link>
-                            
-                <p>CLick here to see what I have created with WordPress</p>
-                <a href="https://rohan-chauhan.com" className="primary-btn">WordPress Portfolio</a>
+                    <p>Click below to see what I have created with WordPress</p>
+                    
+                    <a href="https://rohan-chauhan.com" className="secondary-btn">WordPress Portfolio</a>
+                </div>
+
             </div>
-        </div>
         )
     }
 }
