8e44633c337079de9be01a0cfa343d4cfce05776
Rohan <rohanpschauhan@gmail.com>
2019-10-14 23:24:42 +0100

    added build
    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.430276fc33da37aba326f104095565d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c56779ee6700bae2a43f9eea9e3f70",
+    "url": "/OC-P4-Portfolio/index.html"
+  },
+  {
+    "revision": "d4c7981cf8f27567a712",
+    "url": "/OC-P4-Portfolio/static/css/main.7a7c0ade.chunk.css"
+  },
+  {
+    "revision": "63df4989dd4b3ac85a1b",
+    "url": "/OC-P4-Portfolio/static/js/2.198ca186.chunk.js"
+  },
+  {
+    "revision": "d4c7981cf8f27567a712",
+    "url": "/OC-P4-Portfolio/static/js/main.a182fb1f.chunk.js"
+  },
+  {
+    "revision": "faefd428e879cb30256c",
+    "url": "/OC-P4-Portfolio/static/js/runtime-main.ce0e3b2f.js"
+  },
+  {
+    "revision": "c843c1cb9346cedcb2ac41c6bd513eb7",
+    "url": "/OC-P4-Portfolio/static/media/BOFF.c843c1cb.png"
+  },
+  {
+    "revision": "7f9c3fdf6284a1d9cad1adcfad7c206a",
+    "url": "/OC-P4-Portfolio/static/media/Score.7f9c3fdf.png"
+  },
+  {
+    "revision": "f789f96c80de5fc7504589e8def9d18c",
+    "url": "/OC-P4-Portfolio/static/media/Tribute.f789f96c.jpg"
+  },
+  {
+    "revision": "a6cd84da1fa2626ff95c79c71d9fc78c",
+    "url": "/OC-P4-Portfolio/static/media/hair.a6cd84da.png"
+  },
+  {
+    "revision": "c1deeef87ed0c70d4e08eb89272c06af",
+    "url": "/OC-P4-Portfolio/static/media/realestate.c1deeef8.png"
+  },
+  {
+    "revision": "09f7e18ae0c6c7455242a78b01f11dd6",
+    "url": "/OC-P4-Portfolio/static/media/rohan.09f7e18a.jpg"
+  }
+]);
